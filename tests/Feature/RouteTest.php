a88fa8add91516e74b5dfc11056620f57bbb4840
xiaohui lam <xiaohui.lam@icloud.com>
2020-09-19 15:36:07 +0800

    init phpunit

---
+++ tests/Feature/RouteTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RouteTest extends TestCase
+{
+    /**
+     * 测试路由是否无法找到
+     *
+     * @return void
+     */
+    public function testRouteExists()
+    {
+        $this->assertTrue(!!route('file.upload'));
+        $this->assertTrue(!!route('user.info.agent'));
+        $this->assertTrue(!!route('conversation.list.agent'));
+        $this->assertTrue(!!route('conversation.message.list.agent', 1));
+        $this->assertTrue(!!route('conversation.message.send.agent', 1));
+        $this->assertTrue(!!route('institution.profile.show'));
+        $this->assertTrue(!!route('institution.profile.update'));
+        $this->assertTrue(!!route('login'));
+        $this->assertTrue(!!route('verification.verify', 1));
+        $this->assertTrue(!!route('oauth.callback', 1));
+        $this->assertTrue(!!route('visitor.init'));
+        $this->assertTrue(!!route('visitor.list-message', 1));
+        $this->assertTrue(!!route('visitor.send-message', 1));
+    }
+}
