a728503b593d577652f68ec0b0e4838767f3008b
xiaohui lam <xiaohui.lam@icloud.com>
2020-09-19 17:16:15 +0800

    测试登录

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    public function testLoginOk()
+    {
+        $this->artisan('migrate');
+        $this->artisan('db:seed', [ '--class' => \Database\Seeders\PermissionSeeder::class])->assertExitCode(0);
+        $this->artisan('db:seed', [ '--class' => \Database\Seeders\AdminSeeder::class])->assertExitCode(0);
+
+        $login = $this->post(route('login', [], false), ['email' => 'admin@admin.com', 'password' => '123456']);
+        $login->assertStatus(200);
+        $login->assertJsonPath('success', true);
+        $this->assertNotEmpty($login->json('data'));
+        $this->assertNotEmpty($login->json('data.user'));
+        $this->assertNotEmpty($login->json('data.institution'));
+        $this->assertArrayHasKey('token', $login->json('data'));
+        $login->assertJsonPath('data.token_type', 'Bearer');
+    }
+
+    public function testLoginWrong()
+    {
+        $this->artisan('migrate');
+        $this->artisan('db:seed', [ '--class' => \Database\Seeders\PermissionSeeder::class])->assertExitCode(0);
+        $this->artisan('db:seed', [ '--class' => \Database\Seeders\AdminSeeder::class])->assertExitCode(0);
+
+        $login = $this->post(route('login', [], false), ['email' => 'admin@admin.com', 'password' => '654321']);
+        $login->assertStatus(422);
+    }
+}
