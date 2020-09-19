de0d32bc6ef5dd6fd2304698aae141a115198795
xiaohui lam <xiaohui.lam@icloud.com>
2020-09-19 17:05:41 +0800

    测试互发信息能否看到

--- tests/Feature/MessageTest.php
+++ tests/Feature/MessageTest.php
@@ -59,7 +59,53 @@ class MessageTest extends ConversationTest
         $this->assertTrue($sendConversationRes->json('success'));
         $this->assertNotEmpty($sendConversationRes->json('data.message'));
 
+        $getConversationRes = $this->get(route('visitor.list-message', [$conversation_id,], false), $this->authVisitor());
+        $getConversationRes->assertStatus(200);
+        $this->assertNotEmpty($getConversationRes->json('data.messages'));
+        $this->assertContains($message, collect($getConversationRes->json('data.messages'))->pluck('content'));
+    }
+
+    /**
+     * 测试访客发的信息，客服能否看到
+     */
+    public function testAgentSeeVisitorMessage()
+    {
+        $conversationRes = $this->testConversationsFromSeeders();
+        $conversation_id = $conversationRes->json('data.conversation.id');
+
+        $message = Str::random();
+        $sendConversationRes = $this->post(route('visitor.send-message', [$conversation_id], false), [
+            'type' => Message::TYPE_TEXT,
+            'content' => $message,
+        ], $this->authVisitor());
+        $sendConversationRes->assertStatus(200);
+        $this->assertTrue($sendConversationRes->json('success'));
+        $this->assertNotEmpty($sendConversationRes->json('data.message'));
+
         $getConversationRes = $this->get(route('conversation.message.list.agent', [$conversation_id,], false), $this->authManager());
+        $getConversationRes->assertStatus(200);
+        $this->assertNotEmpty($getConversationRes->json('data.messages'));
+        $this->assertContains($message, collect($getConversationRes->json('data.messages'))->pluck('content'));
+    }
+
+    /**
+     * 测试客服发的信息，访客能否看到
+     */
+    public function testVisitorSeeAgentMessage()
+    {
+        $conversationRes = $this->testConversationsFromSeeders();
+        $conversation_id = $conversationRes->json('data.conversation.id');
+
+        $message = Str::random();
+        $sendConversationRes = $this->post(route('conversation.message.send.agent', [$conversation_id], false), [
+            'type' => Message::TYPE_TEXT,
+            'content' => $message,
+        ], $this->authManager());
+        $sendConversationRes->assertStatus(200);
+        $this->assertTrue($sendConversationRes->json('success'));
+        $this->assertNotEmpty($sendConversationRes->json('data.message'));
+
+        $getConversationRes = $this->get(route('visitor.list-message', [$conversation_id,], false), $this->authVisitor());
         $getConversationRes->assertStatus(200);
         $this->assertNotEmpty($getConversationRes->json('data.messages'));
         $this->assertContains($message, collect($getConversationRes->json('data.messages'))->pluck('content'));
