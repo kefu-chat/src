5849e439e23b6ccec3572375b25e580f2fb9a21f
xiaohui.lam <xiaohui.lam@icloud.com>
2020-09-20 13:54:58 +0800

    fixbug

--- routes/channels.php
+++ routes/channels.php
@@ -12,13 +12,22 @@
 */
 
 use App\Models\Conversation;
+use App\Models\Message;
 use App\Models\User;
 use App\Models\Visitor;
+use Illuminate\Support\Arr;
+use Vinkla\Hashids\Facades\Hashids;
 
 /**
  * @see \App\Broadcasting\ConversationMessaging
  */
 Broadcast::channel('conversation.{id}.messaging', function ($user, $id) {
+    $decoded = Hashids::decode($id);
+    if (Arr::last($decoded) != crc32(Conversation::class)) {
+        abort(403, '路由 ID 拼错了， CRC32校验失败: ' . Conversation::class);
+    }
+    $id = Arr::first($decoded);
+
     $conversation = Conversation::findOrFail($id);
     return $conversation->{[Visitor::class => 'visitor_id', User::class => 'user_id'][get_class($user)]} == $user->id;
 }, ['guards' => ['visitor', 'api']]);
