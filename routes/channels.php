3deb70748a5819bb0c8587e627edaaa29a997acf
xiaohui lam <xiaohui.lam@icloud.com>
2020-09-23 14:42:43 +0800

    解决 presence joining 和 leaving 的问题

--- routes/channels.php
+++ routes/channels.php
@@ -11,6 +11,7 @@
 |
 */
 
+use App\Http\Transformers\ConversationUserTransformer;
 use App\Models\Conversation;
 use App\Models\Enterprise;
 use App\Models\Institution;
@@ -23,13 +24,25 @@ use Spatie\Permission\Models\Permission;
  * @see \App\Broadcasting\ConversationAssigning
  */
 Broadcast::channel('conversation.{conversation}', function ($user, Conversation $conversation) {
-    return $conversation->{[Visitor::class => 'visitor_id', User::class => 'user_id'][get_class($user)]} == $user->id;
+    if ($conversation->{[Visitor::class => 'visitor_id', User::class => 'user_id'][get_class($user)]} == $user->id) {
+        if ($user instanceof User) {
+            return $user->setTransformer(ConversationUserTransformer::class);
+        }
+        return $user;
+    }
+    return false;
 }, ['guards' => ['visitor', 'api']]);
 
 Broadcast::channel('institution.{institution}', function (User $user, Institution $institution) {
-    return $institution->id == $user->institution_id || ($user->hasPermissionTo(Permission::findByName('manager', 'api')) && $institution->enterprise_id = $user->enterprise_id);
+    if ($institution->id == $user->institution_id || ($user->hasPermissionTo(Permission::findByName('manager', 'api')) && $institution->enterprise_id = $user->enterprise_id)) {
+        return $user->setTransformer(ConversationUserTransformer::class);
+    }
+    return false;
 }, ['guards' => ['api']]);
 
 Broadcast::channel('enterprise.{institution}', function (User $user, Enterprise $enterprise) {
-    return $user->hasPermissionTo(Permission::findByName('manager', 'api')) && $enterprise->id = $user->enterprise_id;
+    if ($user->hasPermissionTo(Permission::findByName('manager', 'api')) && $enterprise->id = $user->enterprise_id) {
+        return $user->setTransformer(ConversationUserTransformer::class);
+    }
+    return false;
 }, ['guards' => ['api']]);
