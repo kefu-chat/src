5fa0dfb5e5e2b16d35e037d167aad126cf60a52c
xiaohui.lam <xiaohui.lam@icloud.com>
2020-09-29 00:30:31 +0800

    fixbug

--- database/migrations/2020_09_19_050919_create_permission_tables.php
+++ database/migrations/2020_09_19_050919_create_permission_tables.php
@@ -37,7 +37,7 @@ class CreatePermissionTables extends Migration
         Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames) {
             $table->unsignedBigInteger('permission_id');
 
-            $table->string('model_type');
+            $table->string('model_type', 90);
             $table->unsignedBigInteger($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
 
@@ -53,7 +53,7 @@ class CreatePermissionTables extends Migration
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames) {
             $table->unsignedBigInteger('role_id');
 
-            $table->string('model_type');
+            $table->string('model_type', 90);
             $table->unsignedBigInteger($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_roles_model_id_model_type_index');
 
