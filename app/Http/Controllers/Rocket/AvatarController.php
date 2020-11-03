afe42650720aec5509eca7c20a7c24fa9d3fd69b
xiaohui lam <xiaohui.lam@icloud.com>
2020-11-03 13:12:36 +0800

    fix

---
+++ app/Http/Controllers/Rocket/AvatarController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Http\Controllers\Rocket;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AvatarController extends Controller
+{
+    public function admin(Request $request)
+    {
+        return '';
+    }
+
+    public function general(Request $request)
+    {
+        return '';
+    }
+
+    public function chat(Request $request)
+    {
+        return '';
+    }
+}
