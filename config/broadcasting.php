cf38ae8a2ca3b6b87e79a4afbb2e10db7fc74903
xiaohui lam <xiaohui.lam@icloud.com>
2020-11-06 15:08:25 +0800

    fixbug

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'host' => env('PUSHER_APP_HOST'),
+                'port' => env('PUSHER_APP_PORT'),
+                'useTLS' => false,
             ],
         ],
 
