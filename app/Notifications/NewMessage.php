e654f2362cc9ad41d8255a6c19a4889a16a7fe96
xiaohui lam <xiaohui.lam@icloud.com>
2020-12-09 15:46:21 +0800

    fix data structure

--- app/Notifications/NewMessage.php
+++ app/Notifications/NewMessage.php
@@ -11,6 +11,7 @@ use Illuminate\Notifications\Notification;
 use NotificationChannels\WebPush\HasPushSubscriptions;
 use NotificationChannels\WebPush\WebPushMessage;
 use NotificationChannels\WebPush\WebPushChannel;
+use Xiaohuilam\Laravel\WxappNotificationChannel\Broadcasting\WechatAppNotificationChannel;
 use Xiaohuilam\Laravel\WxappNotificationChannel\Interfaces\WechatAppNotificationable;
 
 class NewMessage extends Notification implements ShouldQueue, WechatAppNotificationable
@@ -34,7 +35,7 @@ class NewMessage extends Notification implements ShouldQueue, WechatAppNotificat
      */
     public function via($notifiable)
     {
-        return $notifiable instanceof User ? [WebPushChannel::class, 'wechat-app'] : [WebPushChannel::class];
+        return $notifiable instanceof User ? [WebPushChannel::class, WechatAppNotificationChannel::class] : [WebPushChannel::class];
     }
 
     /**
@@ -76,10 +77,18 @@ class NewMessage extends Notification implements ShouldQueue, WechatAppNotificat
     public function getTemplateMessageData()
     {
         return [
-            'name1' => $this->notification->sender->name, //客户名称
-            'time2' => $this->notification->created_at->format('Y年m月d日 H:i:s'), //咨询时间
-            'thing3' => $this->notification->type == Message::TYPE_TEXT ? $this->notification->content : '[客户发来一张图片]', //咨询内容
-            'phone_number5' => optional(optional(optional($this->notification)->conversation)->visitor)->phone ?: '未获得手机号', //客户手机号
+            'name1' => [ //客户名称
+                'value' => $this->notification->sender->name,
+            ],
+            'time2' => [ //咨询时间
+                'value' => $this->notification->created_at->format('Y年m月d日 H:i:s'),
+            ],
+            'thing3' => [ //咨询内容
+                'value' => $this->notification->type == Message::TYPE_TEXT ? $this->notification->content : '[图片]',
+            ],
+            'phone_number5' => [ //客户手机号
+                'value' => optional(optional(optional($this->notification)->conversation)->visitor)->phone ?: '未获得手机号',
+            ],
         ];
     }
 
